0aee942771648e9abb95fcc7b99f9a08f0563b3b
IsaiasPC <isamed92@gmail.com>
2019-06-03 01:10:54 -0300

    first commit login created w angular validation and angular form template

---
+++ src/app/pages/registro/registro.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { UsuarioModel } from 'src/app/models/usuario.model';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-registro',
+  templateUrl: './registro.component.html',
+  styleUrls: ['./registro.component.css']
+})
+export class RegistroComponent implements OnInit {
+
+  usuario: UsuarioModel;
+
+  constructor() { }
+
+  ngOnInit() {
+    this.usuario = new UsuarioModel();
+    // this.usuario.email = 'isamed92@gmail.com'
+   }
+
+   onSubmit(form: NgForm) {
+     if(form.invalid) return;
+     console.log('Formulario enviado');
+     console.log(this.usuario);
+     console.log(form);
+     
+   }
+
+}
